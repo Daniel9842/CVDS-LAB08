b562cd3d363ecd4dc4c9eb6090aad55a31f2568b
Miguel Rodríguez <miguel.rodriguez-s@mail.escuelaing.edu.co>
2020-10-11 21:27:50 -0500

    LAB-08

--- src/test/java/eci/cvds/test/ServiciosAlquilerTest.java
+++ src/test/java/eci/cvds/test/ServiciosAlquilerTest.java
@@ -8,6 +8,7 @@ import edu.eci.cvds.sampleprj.dao.PersistenceException;
 import edu.eci.cvds.samples.entities.Cliente;
 import edu.eci.cvds.samples.entities.Item;
 import edu.eci.cvds.samples.entities.ItemRentado;
+import edu.eci.cvds.samples.entities.TipoItem;
 import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.cvds.samples.services.ServiciosAlquiler;
 import edu.eci.cvds.samples.services.ServiciosAlquilerFactory;
@@ -16,6 +17,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.text.SimpleDateFormat;
 
 public class ServiciosAlquilerTest {
 
@@ -46,5 +48,63 @@ public class ServiciosAlquilerTest {
             // Validate no Client was found;
             Assert.assertTrue(r);
         };
+    }
+	
+	@Test
+    public void clienteOK() {
+        try {
+            Cliente cliente = serviciosAlquiler.consultarCliente(54321);
+        } catch (ExcepcionServiciosAlquiler e) {
+            Assert.assertEquals(e.getMessage(), "Error al consultar el cliente con identificacion 54321");
+        }
+    }
+	
+	@Test
+    public void clienteNoOK() {
+        try {
+            Cliente cliente = serviciosAlquiler.consultarCliente(6996);
+        } catch (ExcepcionServiciosAlquiler e) {
+            Assert.assertEquals(e.getMessage(), "Error al consultar el cliente con identificacion 6996");
+        }
+    }
+	
+	@Test
+    public void clienteDuplicadoOk() {
+        try {
+            Cliente cliente = serviciosAlquiler.consultarCliente(699601231);
+            serviciosAlquiler.registrarCliente(cliente);
+        } catch (ExcepcionServiciosAlquiler e) {
+            Assert.assertEquals(e.getMessage(), "Error al consultar el cliente con identificacion 699601231");
+        }
+	}
+	
+	@Test
+    public void clienteDuplicadoNoOk() {
+        try {
+            Cliente cliente = serviciosAlquiler.consultarCliente(6996);
+            serviciosAlquiler.registrarCliente(cliente);
+        } catch (ExcepcionServiciosAlquiler e) {
+            Assert.assertEquals(e.getMessage(), "Error al consultar el cliente con identificacion 6996");
+        }
+	}
+	
+	@Test
+	public void registrarItemOK(){
+        try {
+            serviciosAlquiler.registrarItem(new Item(new TipoItem(8899, "Celuco1"), 489212, "Celular Samsung", "Se llama y se chatea baby", new SimpleDateFormat("yyyy/MM/dd").parse("2010/11/12"),350000, "Tecnologia", "Celuco1"));
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+	
+	@Test
+	public void registrarItemNoOK(){
+        try {
+            serviciosAlquiler.registrarItem(new Item(new TipoItem(8899, "Celuco"), 48921, "Celular Samsung", "Se llama y se chatea baby", new SimpleDateFormat("yyyy/MM/dd").parse("2010/11/12"),350000, "Tecnologia", "Celuco1"));
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
